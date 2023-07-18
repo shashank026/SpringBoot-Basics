bcf1993134eb6d364a125e497401604dc044d7be
Shashank <72060531+shashank026@users.noreply.github.com>
2023-07-18 20:54:42 +0530

    first commit

---
+++ course-api-data/src/main/java/com/example/demo/topic/TopicRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo.topic;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface TopicRepository extends CrudRepository<Topic, String> {
+	
+	
+	 
+}
